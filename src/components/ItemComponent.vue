ba6c8d9e26952962d5126d019e3d3ccddbfbc553
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2018-11-25 16:07:13 +0100

    Added line-through

--- src/components/ItemComponent.vue
+++ src/components/ItemComponent.vue
@@ -2,7 +2,9 @@
   <li :class="{'removed':item.checked}">
     <div class="custom-control custom-checkbox">
       <input type="checkbox" v-model="item.checked" class="custom-control-input" :id="item.id" />
-      <label class="custom-control-label" :for="item.id">{{ item.text }}</label>
+      <label class="custom-control-label"
+             :for="item.id"
+             :style="{textDecoration: item.checked ? 'line-through' : 'none'}">{{ item.text }}</label>
     </div>
   </li>
 </template>
--- src/components/ShoppingListPlain.vue
+++ src/components/ShoppingListPlain.vue
@@ -15,7 +15,9 @@
       <li v-for="item in items" :key="item.id" :class="{'removed':item.checked}">
         <div class="custom-control custom-checkbox">
           <input type="checkbox" v-model="item.checked" class="custom-control-input" :id="item.id" />
-          <label class="custom-control-label" :for="item.id">{{ item.text }}</label>
+          <label class="custom-control-label"
+                 :for="item.id"
+                 :style="{textDecoration: item.checked ? 'line-through' : 'none'}">{{ item.text }}</label>
         </div>
       </li>
     </ul>
