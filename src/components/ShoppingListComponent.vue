f1948925fed56791d33747025a6d4fd0475699be
cichy380 <marcin.dobroszek@gmail.com>
2018-11-27 09:24:25 +0100

    Added event 'addItem'

--- src/components/ShoppingListComponent.vue
+++ src/components/ShoppingListComponent.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="shopping-list mt-4">
     <h2 class="mb-4">{{ title }}</h2>
-    <add-item-component></add-item-component>
+
+    <add-item-component @add="addItem"></add-item-component>
+
     <items-component :items="items"></items-component>
   </div>
 </template>
@@ -12,6 +14,19 @@ import ItemsComponent from './ItemsComponent'
 
 export default {
   components: { AddItemComponent, ItemsComponent },
-  props: ['title', 'items']
+  props: ['title', 'items'],
+  methods: {
+    addItem: function (text) {
+      this.items.push({
+        id: this._generateId(),
+        text: text,
+        checked: false
+      })
+    },
+
+    _generateId: function () {
+      return 'sl-' + Math.random().toString(36).substr(2, 9)
+    }
+  }
 }
 </script>
