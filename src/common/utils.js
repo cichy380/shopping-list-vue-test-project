1f7ff0f6fbce23bc4dfe5cdb803d2044f7386a01
cichy380 <marcin.dobroszek@gmail.com>
2018-12-13 11:21:06 +0100

    Improved change_title and add_item mutation

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,16 +1,3 @@
 export function generateId () {
   return 'id-' + Math.random().toString(36).substr(2, 9)
 }
-export function findWhere (arrayOfObjects, searchedProp) {
-  let searchedObject = {}
-  let searchedPropName = Object.keys(searchedProp)[0]
-  let searchedPropValue = searchedProp[searchedPropName]
-
-  arrayOfObjects.forEach((obj) => {
-    if (obj[searchedPropName] === searchedPropValue) {
-      searchedObject = obj
-    }
-  })
-
-  return searchedObject
-}
--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -1,12 +1,12 @@
-import { generateId, findWhere } from './../common/utils'
+import { generateId } from './../common/utils'
 import * as types from './mutation_types'
 
 export default {
   [types.CHANGE_TITLE] (state, data) {
-    findWhere(state.shoppinglists, {id: data.id}).title = data.title
+    state.shoppinglists.find(list => list.id === data.id).title = data.title
   },
   [types.ADD_ITEM] (state, data) {
-    findWhere(state.shoppinglists, {id: data.id}).items.push({
+    state.shoppinglists.find(list => list.id === data.id).items.push({
       id: generateId(),
       text: data.item,
       checked: false
